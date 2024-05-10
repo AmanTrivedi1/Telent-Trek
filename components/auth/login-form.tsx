3fbd42b20dc0e3cae7e5bc175266521e091b1f92
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-05-11 01:14:43 +0530

    success remove

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -42,7 +42,6 @@ export const LoginForm = () => {
             login(values)
               .then((data)=>{  
                setError(data?.error );
-               setSuccess(data?.success);
               })
         });
     }
